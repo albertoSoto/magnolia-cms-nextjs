94609ca3a031cf2a18576be6d94b52821fb5849f
Alberto Soto <>
2022-04-22 12:50:00 +0200

    project base

---
+++ nextjs-poc-module/src/main/java/com/albertosoto/mgnl/NextJSPocModule.java
@@ -0,0 +1,13 @@
+package com.albertosoto.mgnl;
+
+/**
+ * This class is optional and represents the configuration for the nextjs-poc-module module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/nextjs-poc-module</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ * See https://documentation.magnolia-cms.com/display/DOCS/Module+configuration for information about module configuration.
+ */
+public class NextJSPocModule {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
